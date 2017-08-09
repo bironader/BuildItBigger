ac31008c7f2f4b6ab6d10b02811a19172830d53e
Biro <bironader123@gmail.com>
2017-08-09 21:30:48 +0200

    Add admob

--- app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
@@ -2,21 +2,74 @@ package com.udacity.gradle.builditbigger.free;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.udacity.gradle.builditbigger.EndPointAsyncTask;
 import com.udacity.gradle.builditbigger.R;
 
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private AdView mAdView;
+    private Button tellJoke;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tellJoke = (Button) findViewById(R.id.tellJoke);
+        mAdView = (AdView) findViewById(R.id.adView);
+//
+//        final InterstitialAd mInterstitialAd = new InterstitialAd(this);
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+
+        final InterstitialAd mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId(getString(R.string.banner_ad_unit_id));
+        mInterstitialAd.loadAd(adRequest);
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                if (!mInterstitialAd.isLoaded()) {
+                    AdRequest adRequest = new AdRequest.Builder()
+                            .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                            .build();
+                    mInterstitialAd.loadAd(adRequest);
+                }
+
+                new EndPointAsyncTask(MainActivity.this).execute();
+
+            }
+        });
+
+        tellJoke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (mInterstitialAd.isLoaded()) {
+                    mInterstitialAd.show();
+
+                } else
+                    new EndPointAsyncTask(MainActivity.this).execute();
+            }
+        });
+
+
     }
 
 
@@ -41,13 +94,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void tellJoke(View view) {
-            new EndPointAsyncTask(this).execute();
-    }
-
-
-
 
 
 }
